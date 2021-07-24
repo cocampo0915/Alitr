30698170942160180ac647d75235e9d0ff4554b5
jassmin-herr <jherr0905@gmail.com>
2021-07-24 00:22:01 -0500

    changes to models

--- alitr/main_app/models.py
+++ alitr/main_app/models.py
@@ -1,42 +1,54 @@
-from django.db import models
-from django.contrib.auth.models import User
-
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse #matching url
 
-
 # Create your models here.
 class Job_application(models.Model):
-    job_title = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
     location = models.CharField(max_length=250)
-    date = models.DateField('date applied')
-    requirements = models.TextField(max_length=500)
-    notes = models.TextField(max_length=500)
+    date = models.DateField('Date Applied')
+    url = models.CharField(
+        max_length=250,
+        blank=True
+        )
+    requirements = models.TextField(
+        max_length=500,
+        blank=True
+        )
+    notes = models.TextField(
+        max_length=500,
+        blank=True
+        )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'job_id': self.id})#every model instance has its own method 
+        return reverse('detail', kwargs={'job_id': self.id}) #every model instance has its own method 
         #django will now render the detail page, 
 
+class Attachment(models.Model): # will implement later with AWS if possible -CO
+    url = models.CharField(max_length=250)
+    job_app = models.ForeignKey(Job_application, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Attachment for job_id: {self.job_id} @{self.url}'
 
 class Status(models.Model):
-  date = models.DateField('date')
-  STATUS = (
-      ('1', 'Applied'),
-      ('2', 'In Progress'),
-      ('3', 'Interview scheduled'),
-      ('4', 'Interview complete'),
-      ('5', 'Job Offer'),
-      ('6', 'Incomplete'),
-      ('7', 'Not Selected'),
-      ('8', 'Unknown'),
+  date = models.DateField('Date')
 
+  STATUS = (
+        ('A', 'Applied'),
+        ('B', 'In Progress'),
+        ('C', 'Interview scheduled'),
+        ('D', 'Interview complete'),
+        ('E', 'Job Offer'),
+        ('F', 'Incomplete'),
+        ('G', 'Not Selected'),
+        ('H', 'Unknown'),
   )
  
   status = models.CharField(
@@ -49,8 +61,10 @@ class Status(models.Model):
 
   def __str__(self):
     # Nice method for obtaining the friendly value of a Field.choice
-    return f"{self.get_status_display()} on {self.date}"
+    return f"{self.get_status_display()}"
 
   # change the default sort
   class Meta:
     ordering = ['-date']
+
+# placeholder for Company model if possible -CO
